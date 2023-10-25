a99d1d7ae4b675b882953b0f68459ea2497c0866
changwanling <wcha305@aucklanduni.ac.nz>
2023-10-26 09:45:13 +1300

    add test table sql back, references test-dao

--- sql/project-database-init-script.sql
+++ sql/project-database-init-script.sql
@@ -1,3 +1,22 @@
+/*
+ * Upon submission, this file should contain the SQL script to initialize your database.
+ * It should contain all DROP TABLE and CREATE TABLE statments, and any INSERT statements
+ * required.
+ */
+drop table if exists test;
+
+create table test (
+    id integer not null primary key,
+    stuff text  
+);
+
+insert into test (stuff) values
+    ('Things'),
+    ('More things');
+
+/*
+*philanthropic-polar-bears sql starts here
+*/
 DROP TABLE IF EXISTS message;
 DROP TABLE IF EXISTS comment;
 DROP TABLE IF EXISTS image;
