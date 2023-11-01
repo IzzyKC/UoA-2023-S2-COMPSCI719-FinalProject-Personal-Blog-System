6596e66bb40270fd4555d7d73f9c9bf5e41b554c
changwanling <wcha305@aucklanduni.ac.nz>
2023-11-02 12:13:16 +1300

    add main page link log-in

---
+++ routes/user-routes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/login", function (req, res) {
+
+    if (res.locals.user) {
+        res.redirect("/");
+    }
+
+    else {
+        res.render("login");
+    }
+
+});
+
+router.post("/login", async function (req, res) {
+
+    // Get the username and password submitted in the form
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Find a matching user in the database
+    const user = await userDao.retrieveUserWithCredentials(username, password);
+
+    // if there is a matching user...
+    if (user) {
+        // Auth success - give that user an authToken, save the token in a cookie, and redirect to the homepage.
+        const authToken = uuid();
+        user.authToken = authToken;
+        await userDao.updateUser(user);
+        res.cookie("authToken", authToken);
+        res.locals.user = user;
+        res.redirect("/");
+    }
+
+    // Otherwise, if there's no matching user...
+    else {
+        // Auth fail
+        res.locals.user = null;
+        res.setToastMessage("Authentication failed!");
+        res.redirect("./login");
+    }
+});
+
+
+module.exports = router
